20b9ba06ec5ad58f9d4c0592587c9516b1e5184a
KirillRib <kirillribkin1998@gmail.com>
2020-01-25 12:39:58 +0300

    Добавлен файл конфигураций

---
+++ config.py
@@ -0,0 +1,13 @@
+import json
+
+
+__json = json.load(open('config.json', 'r'))
+
+TEST_PATH = __json.get('test_path')
+TRAIN_PATH = __json.get('train_path')
+SUBMISSION_CVS = __json.get('submission_csv')
+
+TEST_SIZE = __json.get('test_size')
+BATCH_SIZE = __json.get('batch_size')
+
+SEED = 7
