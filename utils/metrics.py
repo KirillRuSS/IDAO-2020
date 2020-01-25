0169a1d63c3238168bc46a4529af18701dd1051a
KirillRib <kirillribkin1998@gmail.com>
2020-01-26 01:10:14 +0300

    Добавленны метрики

---
+++ utils/metrics.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def smape(satellite_predicted_values: np.array, satellite_true_values: np.array) -> float:
+    """
+    Симметричная средняя абсолютная процентная ошибка
+    :param satellite_predicted_values: предсказанное значение
+    :param satellite_true_values: истинное значение
+    :return: величина ошибки
+    """
+    return np.mean(np.abs((satellite_predicted_values - satellite_true_values)
+                          / (np.abs(satellite_predicted_values) + np.abs(satellite_true_values))))
+
+
+def score(satellite_predicted_values: np.array, satellite_true_values: np.array) -> float:
+    """
+    Скор на лидерборде
+    :param satellite_predicted_values: предсказанное значение
+    :param satellite_true_values: истинное значение
+    :return: скор
+    """
+    return 100 * (1 - smape(satellite_predicted_values, satellite_true_values))
