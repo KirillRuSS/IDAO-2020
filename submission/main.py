6562d512b5e922da97ff3c65b53fc6ec6699beb3
KirillRib <kirillribkin1998@gmail.com>
2020-02-01 22:21:44 +0300

    Исправленно оформление ноутбука

--- submission/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-test = pd.read_csv("test.csv")
-
-# just sending simulated values as the answer
-submission = test[["id", "x_sim", "y_sim", "z_sim", "Vx_sim", "Vy_sim", "Vz_sim"]]
-submission.columns = ["id", "x", "y", "z", "Vx", "Vy", "Vz"]
-submission.to_csv("submission.csv", index=False)
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import keras.backend as K
 
 
 def smape(satellite_predicted_values: np.array, satellite_true_values: np.array) -> float:
@@ -20,3 +21,15 @@ def score(satellite_predicted_values: np.array, satellite_true_values: np.array)
     :return: скор
     """
     return 100 * (1 - smape(satellite_predicted_values, satellite_true_values))
+
+
+def smape_loss():
+    """
+    Функция, для передачи метрики керасу
+    :return: функция потерь
+    """
+    def loss(satellite_predicted_values, satellite_true_values):
+        return K.mean(K.abs((satellite_predicted_values - satellite_true_values)
+                            / (K.abs(satellite_predicted_values) + K.abs(satellite_true_values))))
+
+    return loss
